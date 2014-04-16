4bb86b2413a44af164124a294e55b9f90ee77920
Jacob Wolniewicz <jacob@wolniewicz.com>
2014-04-15 19:04:59 -0600

    initial code

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+app = Flask(__name__)
+
+@app.route('/', methods=['POST', 'GET'])
+def index ():
+    name = request.args.get('name', '')
+    #return "Hello " + name
+    return render_template('home.html', name=name)
+
+@app.route('/robot')
+@app.route('/view/go/<distance>')
+def view(distance=None):
+	return render_template('robot.html', distance=distance)
+   
+
+
+
+if __name__ == '__main__':
+    app.run(debug = true)
